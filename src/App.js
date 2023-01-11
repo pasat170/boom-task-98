eb9a12fefebcdebece506647953f44c49b020733
Stefan Poibrenski <stefan_poibrenski@abv.bg>
2023-01-11 22:53:07 +0200

    feat: add login form component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
+    return (
+        <div className="App">
+            <LoginForm />
         </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
